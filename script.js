cb8ab234eafd090dba93b376d9823e3821c021bd
rishwanthraam <rishwanthraam392006@gmail.com>
2025-11-07 13:10:16 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var menuOpenButton = document.querySelector("#open-menu-bar")
+
+menuOpenButton.addEventListener("click", () => {
+    //toggle represent show in the mobile menu visibility
+    document.body.classList.toggle ("show-mobile-menu");
+}) ;
